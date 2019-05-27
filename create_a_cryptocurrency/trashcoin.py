b909d0d73b905c3991fa80accce9674c0e8b7d1e
SverreSB <sverre.sb@gmail.com>
2019-05-27 12:19:10 +0200

    created api for replacing chain to a longer chain

--- create_a_cryptocurrency/trashcoin.py
+++ create_a_cryptocurrency/trashcoin.py
@@ -188,4 +188,20 @@ def connect_node():
 
     return jsonify(response), 201
 
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {
+            'message': 'The nodes had different chains so the chain was replaced by the longer chain',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'All good. The chain is the largest one.',
+            'actual_chain': blockchain.chain
+            }
+    
+    return jsonify(response), 200
+
 app.run(host = '0.0.0.0', port = 5000)
